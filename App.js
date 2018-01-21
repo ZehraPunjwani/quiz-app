852dff34688f1c66199ece9b5e9d03a7a94f2e88
Zehra Punjwani <punjwanizee@gmail.com>
2018-01-21 14:44:56 +0000

    Complete :)

--- App.js
+++ App.js
@@ -1,23 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import DeckList from './components/DeckList'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+import { FontAwesome, MaterialCommunityIcons } from '@expo/vector-icons'
+import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
+import AddDeck from './components/AddDeck'
+import DeckView from './components/DeckView'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+import { createStore } from 'redux'
+import AddCard from './components/AddCard'
+import Quiz from './components/Quiz'
+import { setLocalNotification } from './utils/helpers'
+
+
+
+function MyStatusBar ({backgroundColor, ...props}) {
+  return (
+      <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+          <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+      </View>
+    )
+}
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',
+      tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor}/>
+    }
+  },
+    AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add Deck',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor}/>
+    }
+  }
+}, {
+  tabBarOptions: {
+    activeTintColor: purple,
+    style: {
+      height: 56,
+      backgroundColor: white,
+
+    }
+  }
+})
+
+const MainNavigator = StackNavigator({
+    Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null
+    }
+  },
+  DeckView: {
+    screen: DeckView,
+    navigationOptions: {
+      title: 'Deck Info',
+      headerTintColor: white,
+      headerStyle: {
+        backgroundColor: purple
+      }
+    }
+  },
+  AddCard: {
+    screen: AddCard,
+      navigationOptions: {
+        title: 'Add Card',
+        headerTintColor: white,
+        headerStyle: {
+          backgroundColor: purple
+        }
+      }
+    },
+    Quiz: {
+      screen: Quiz,
+        navigationOptions: {
+          title: 'Quiz',
+          headerTintColor: white,
+          headerStyle: {
+            backgroundColor: purple
+          }
+        }
+    }
+})
+
 
 export default class App extends React.Component {
+
+  componentDidMount(){
+    setLocalNotification()
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+          <View style={{flex: 1}}>
+            <MyStatusBar backgroundColor={purple} barStyle='light-content'/>
+            <MainNavigator/>
+         </View>
+       </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ utils/api.js
@@ -0,0 +1,71 @@
+import { AsyncStorage } from 'react-native'
+
+const FLASHCARDS_STORAGE_KEY = 'flashcards: decks'
+
+const initialData = {
+    Geography: {
+      title: 'Geography',
+      questions: [
+        {
+          question: 'Is South Africa a country?',
+          answer: 'No, it is just a region',
+          correctAnswer: 'false'
+        },
+        {
+          question: 'Which US State is next to California?',
+          answer: 'New York',
+          correctAnswer: 'false'
+        }
+      ] 
+    },
+    JavaScript: {
+      title: 'JavaScript',
+      questions: [
+        {
+          question: 'What is a closure?',
+          answer: 'The combination of a function and the lexical environment within which that function was declared.',
+          correctAnswer: 'true'
+        },
+        {
+          question: 'What is varibale',
+          answer: 'Something that stores information.',
+          correctAnswer: 'true'
+        }
+      ]
+    }
+  }
+
+  export const getData = () => {
+      return initialData;
+  }
+
+  export function getDecks (deck) {
+    return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+    .then(results => {
+      if(results === null) {
+        AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(initialData))
+        return initialData
+      }else {
+        return JSON.parse(results)
+      }
+    })
+  }
+
+  export function saveDeckTitle(title){
+    return AsyncStorage.mergeItem(FLASHCARDS_STORAGE_KEY, JSON.stringify({
+      [title]: {
+        title: title,
+        questions: []
+      }
+    }))
+  }
+
+export function addCardToDeck(name, card){
+  return AsyncStorage.getItem(FLASHCARDS_STORAGE_KEY)
+  .then(results => JSON.parse(results))
+  .then(results => {
+    results[name].questions.push(card)
+    AsyncStorage.setItem(FLASHCARDS_STORAGE_KEY, JSON.stringify(results))
+      return results
+  })
+}
